85a8c1de88f85f76c1bf3050151a75e120ef91af
lolo35 <filimon35@gmail.com>
2017-01-14 03:00:18 +0200

    db_connect
    added requier_once to export.php for easier editing

--- export.php
+++ export.php
@@ -1,8 +1,5 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "Multiread23";
-$dbname = "mydb";
+require_once 'Dbconnect.php';
 
 //Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
